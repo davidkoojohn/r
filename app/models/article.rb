6172ca2f967626aacd25bfe24098050e7f056acc
davidkoojohn <davidkoojohn@gmail.com>
2017-11-04 23:16:06 +0800

    add article validates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 5}
+
+  # Range unspecified. Specify the :in, :within, :maximum, :minimum, or :is option.
+
 end

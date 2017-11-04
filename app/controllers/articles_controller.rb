47ae5190614e8ac9cf6c14b4a42bf78e6ab586b1
davidkoojohn <davidkoojohn@gmail.com>
2017-11-04 22:27:58 +0800

    add creat article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
 
+  def show
+    @article = Article.find(params[:id])
   end
 
   def new
@@ -9,6 +13,13 @@ class ArticlesController < ApplicationController
 
   def create
     #render plain: params.inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
 
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end

868984f9e7da5b7f01c4b90581c71b360295b49e
davidkoojohn <davidkoojohn@gmail.com>
2017-11-05 22:24:48 +0800

    add comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5}
-
   # Range unspecified. Specify the :in, :within, :maximum, :minimum, or :is option.
-
+  has_many :comments
+  
 end

369a9fccaaf50c528a5c9ac0b49b6c09aa7329f1
davidkoojohn <davidkoojohn@gmail.com>
2017-11-08 13:43:25 +0800

    add consumer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107075435) do
+ActiveRecord::Schema.define(version: 20171108053750) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -57,6 +57,23 @@ ActiveRecord::Schema.define(version: 20171107075435) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "consumers", force: :cascade do |t|
+    t.string "phone", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["phone"], name: "index_consumers_on_phone", unique: true
+    t.index ["reset_password_token"], name: "index_consumers_on_reset_password_token", unique: true
   end
 
   create_table "tests", force: :cascade do |t|
